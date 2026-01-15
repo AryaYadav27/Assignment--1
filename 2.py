1f6927a4b6cea6bfb009ffa170a25121b31dcd12
AryaYadav27 <arya.25134@ee.du.ac.in>
2026-01-15 22:09:11 +0530

    Create 2.py

---
+++ 2.py
@@ -0,0 +1,13 @@
+"""
+Arranging clothes in a luggage bag
+
+Step 1: Start
+Step 2: Decide the place and number of days for the trip
+Step 3: Make a list of clothes needed for different days and activities
+Step 4: Separate clothes according to size and type
+Step 5: Roll clothes to save space in the luggage
+Step 6: Keep heavy clothes at the bottom of the bag
+Step 7: Keep daily-use clothes on the top for easy access
+Step 8: Place delicate clothes carefully between other clothes
+Step 9: End
+"""
